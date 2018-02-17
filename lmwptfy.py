d62ebffec1851313c76fe7b4472c67821a908148
Liu-Wen-Yuan <15816141883@163.com>
2018-02-17 22:34:41 +0800

    Update lmwptfy.py

--- lmwptfy.py
+++ lmwptfy.py
@@ -3,3 +3,9 @@ def go():
 w = input("what would you like to know");
 responses.append(w);
 os.system("start \"\" https://en.wikipedia.org/wiki/{w}")
+
+def no_console_input(whattoknow){
+if whattoknow != null:
+ os.system("start \"\" https://en.wikipedia.org/wiki/"+whattoknow)
+
+}
