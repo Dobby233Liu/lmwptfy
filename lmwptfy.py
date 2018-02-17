2948a62f56626fbe375ac9e5d1b9cb2b750cad27
Liu-Wen-Yuan <15816141883@163.com>
2018-02-17 23:25:34 +0800

    Update lmwptfy.py

--- lmwptfy.py
+++ lmwptfy.py
@@ -1,11 +1,11 @@
 import os
 def go():
-w = input("what would you like to know");
-responses.append(w);
-os.system("start \"\" https://en.wikipedia.org/wiki/{w}")
+ w = input("what would you like to know");
+ # windows only
+ os.system("start \"\" https://en.wikipedia.org/wiki/{w}")
 
 def no_console_input(whattoknow):
-if whattoknow != null:
- os.system("start \"\" https://en.wikipedia.org/wiki/"+whattoknow)
+ if whattoknow != "":
+  os.system("start \"\" https://en.wikipedia.org/wiki/"+whattoknow)
 
 
